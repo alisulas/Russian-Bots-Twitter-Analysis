6108c2151fbb46a811b7887402e6464b0049f352
Matt <themattemmons@gmail.com>
2018-02-03 13:36:45 -0500

    Added stream listener for keywords and users (not operational at the moment) that saves the data to mongodb local collection

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 To start, register for an API key at [apps.twitter.com](apps.twitter.com), there is a helpful guide here: [http://social-metrics.org/api-keys/](http://social-metrics.org/api-keys/).
 
-Create a file in the base directory called _config.py_ containing (filling in your consumer key and consumer secret key from [apps.twitter.com](apps.twitter.com)):
+Create a file in the base directory called _config.py_ containing:
 ```
-APP_KEY = 'your app key'
-APP_SECRET = 'your app secret'
+API_KEY = 'twitter API key'
+API_SECRET = 'twitter API secret key'
+ACCESS_TOKEN = 'twitter API access token'
+ACCESS_SECRET = 'twitter API access secret token'
+
+AWS_KEY = 'AWS access key
+AWS_SECRET = 'AWS access secret key'
 ```
 
-Do not share the secret key with anyone!
 
-Finally, run `pip install twython` and you're good to go!
+```
+mongod --port 27017 --dbpath "C:\data\db"
+```
---
+++ scraper.py
@@ -0,0 +1,79 @@
+import json
+import tweepy
+import config as cfg
+from pymongo import MongoClient
+
+MONGO_DB = 'mongodb://localhost:27017/'
+
+USERS = [
+    'smartdissent', 'SparkleSoup45', 'bbusa617', 'charlieJuliet', 'ChrisFromWI',
+    'SCroixFreePress', 'wienerherzog2', 'PeggyRuppe', 'remleona', 'Answers2b4u',
+    'veradubs', 'HarryFerro', 'RussiaInsider', 'Rose4Justice', 'ChristTopNews',
+    'actionScript3', 'willie_c', 'FilAmVA', 'pnehlen', 'Russ_Warrior', 'palestininianpr',
+    '501Wittmann', 'TexanTruth42', 'VNLP2', 'elapoides', 'K1erry', 'justshootme_plz',
+    'starrick1', 'RoseGeorossi', 'MaineFirstMedia', 'Axis__Mundi', 'Imperator_Rex3',
+    'fcfootsteps', 'TrumpArmyDawn', 'TT45pac', 'AMErikaNGIRLBOT', 'gal_deplorable',
+    'bdcousins', 'k_ovfefe', 'drawandstrike', 'antischool_ftw', 'B75434425', 'PatsforTruth',
+    'MollyBly', 'anarcho_commie', 'Mathew_Foresta', 'zalphaprime', 'booatticus63',
+    'USA_H8S_ANTIFA', 'hauzer76', 'SONOFDY1', 'DeceitinDrugs', 'DANEgerus',
+]
+
+KEYWORDS = [
+    'MAGA', 'ReleaseTheMemo', 'Nunes'
+    ]
+
+class TweetListener(tweepy.StreamListener):
+    '''
+    Class that provides listening services to Twitter Streaming API
+    '''
+    def __init__(self, api):
+        '''
+        Initialize Stream Listener Service and connect to DB
+        '''
+        self.api = api
+        super(tweepy.StreamListener, self).__init__()
+        self.db = MongoClient().twitter
+
+    def on_data(self, tweet):
+        '''
+        Save JSON data to DB
+        '''
+        self.db.tweet.insert(json.loads(tweet))
+
+    def on_connect(self):
+        '''
+        Declare proper connection to DB
+        '''
+        print("You are now connected to the streaming API.")
+
+    def on_error(self, status_code):
+        '''
+        Declare error when encountered, do not close stream
+        '''
+        print("Error with code: " + str(status_code))
+        return True
+
+    def on_timeout(self):
+        '''
+        Declare API timeout when encountered, do not close stream
+        '''
+        print("API Timeout")
+        return True
+
+def main():
+    # Authenticate with Twitter API
+    auth = tweepy.OAuthHandler(cfg.API_KEY, cfg.API_SECRET)
+    auth.set_access_token(cfg.ACCESS_TOKEN, cfg.ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    # Setup Listener
+    listener = tweepy.Stream(auth, TweetListener(api))
+
+    # Track tweets from USERS and KEYWORDS
+    # FIXME: need the User ID to follow users, not the username
+    # listener.filter(follow = USERS, track = KEYWORDS)
+    listener.filter(track = KEYWORDS)
+    # listener.filter(follow = USERS)
+
+if __name__ == "__main__":
+    main()
